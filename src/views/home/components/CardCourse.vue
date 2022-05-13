24619ba8310f74222e4f6774f96c7091d980cd70
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2022-05-13 16:10:55 -0300

    Pagina de Listagem Cursos

---
+++ src/views/home/components/CardCourse.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <span :class="['card', 'bg-' + props.bgName]">
+      <span class="icon">
+        <img :src="[props.image != '' ? props.image : require('@/assets/images/icons/default.png')]"
+          :alt="props.title" />
+      </span>
+      <span class="title">{{ props.title }}</span>
+      <span class="action">
+        <a href="trainings.html" :class="['btn', props.bgName]">Acessar</a>
+      </span>
+    </span>
+    <span class="dots">
+      <span></span>
+      <span></span>
+      <span></span>
+    </span>
+  </li>
+</template>
+
+<script setup>
+/*eslint-disable*/
+
+const props = defineProps({
+
+  title: () => [],
+  image: () => [],
+  bgName: () => []
+});
+
+</script>
