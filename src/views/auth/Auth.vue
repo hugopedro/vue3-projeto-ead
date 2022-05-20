be2beb0cae6f52f020685a5a12ec1e2e3f2581be
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2022-05-20 10:54:23 -0300

    Navegação entre telas

--- src/views/auth/Auth.vue
+++ src/views/auth/Auth.vue
@@ -50,7 +50,7 @@
                 <input type="password" name="password" placeholder="Senha" required>
                 <i class="far fa-eye buttom"></i>
               </div>
-              <button class="btn primary" type="submit">Login</button>
+              <button class="btn primary" type="submit" @click.prevent="login">Login</button>
             </form>
             <span>
               <p class="fontSmall">
@@ -69,4 +69,6 @@
 </template>
 
 <script setup>
+import router from '@/router'
+const login = () => router.push({ name: 'campus.home' })
 </script> 
